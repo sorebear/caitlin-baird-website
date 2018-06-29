01684f98b718af2b469d67a197174492ca810d54
Soren Baird <sorenbaird@gmail.com>
2018-06-29 10:45:50 -0700

    Testing Lambda functions

--- lambda-func/testing.js
+++ lambda-func/testing.js
@@ -1,8 +1,6 @@
-var beautify = require('js-beautify');
-
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: `Hello, World! Look at this EVENT: ${beautify(event)} and don't forget the CONTEXt: ${beautify(context)}`
+    body: `Hello, World! Look at this EVENT: ${event} and don't forget the CONTEXt: ${context}`
   })
 }
